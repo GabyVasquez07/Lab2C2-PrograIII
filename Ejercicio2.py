1e35c2dfa3a463d01fd585775cc89088d124916e
sant1ago-quito <santiagokito2000@gmail.com>
2024-10-06 19:43:31 -0600

    Conclusión del ejercicio 2 usando un gráfico de barras

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 #enlace al dataset: https://www.kaggle.com/datasets/pradeepjangirml007/laptop-data-set
 """
 Este dataset contiene especificaciones detalladas de 3.976 portátiles de diversas marcas. 
@@ -8,5 +9,28 @@ opciones de almacenamiento, información de la GPU, tipo de pantalla, precio y d
 Puede resultar útil para explorar y analizar las especificaciones de portátiles de diferentes rangos 
 de precios y configuraciones.
 """
-#mandar a llamar el arcivo
+#mandamos a llamar el arcivo
 df = pd.read_csv("laptop.csv")
+
+# Contamos el número de laptops por marca
+brand_counts = df["Brand"].value_counts()
+
+# Creamos el gráfico de barras
+plt.figure(figsize=(10, 6))  # Ajustamos el tamaño de la gráfica
+brand_counts.plot(kind='bar', color='lightblue')
+#Definimos que queremos un gráfico de barras y que las barras sean color celeste
+
+# Añadimos título y etiquetas
+plt.title("Número de laptops por marca", fontsize=16)
+plt.xlabel("Marca", fontsize=12)
+plt.ylabel("Número de laptops", fontsize=12)
+
+# Rotamos las etiquetas para mayor legibilidad
+plt.xticks(rotation=45, ha="right")
+
+# Ajustamos el diseño
+plt.tight_layout()
+
+# Mostramos gráfico
+plt.show()
+
